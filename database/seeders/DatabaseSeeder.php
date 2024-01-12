921ad2e99364ca805a1596694c15e9edc500462c
Cirque0 <62349517+Cirque0@users.noreply.github.com>
2024-01-12 22:28:56 +0800

    initial migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +19,28 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        DB::table('roles')->insert([
+            'id' => 1,
+            'name' => 'administrator',
+        ]);
+
+
+        DB::table('roles')->insert([
+            'id' => 2,
+            'name' => 'requester',
+        ]);
+
+        \App\Models\User::factory()->create([
+            'role_id' => 1,
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+        ]);
+
+        \App\Models\User::factory()->create([
+            'role_id' => 2,
+            'name' => 'Requester',
+            'email' => 'requester@example.com',
+        ]);
     }
 }
