bdc531ffa84ee26bdee57a142aa3bd3f897a086c
Cirque0 <62349517+Cirque0@users.noreply.github.com>
2024-01-17 22:04:39 +0800

    item card layout change

--- resources/js/Components/Item.jsx
+++ resources/js/Components/Item.jsx
@@ -4,38 +4,38 @@ import { Link } from "@inertiajs/react";
 
 export default function Item({ item }) {
     return (
-        <div className="flex flex-col bg-gray-50 rounded-lg">
-            <div className="flex">
-                <div className="flex flex-col p-4">
-                    <span className="text-xs uppercase tracking-wider">
-                        {item.itemable_type}
-                    </span>
-                    <span className="font-bold">{item.name}</span>
-                    <span className="text-sm">
-                        Acquired{" "}
-                        {new Date(item.acquisition_date).toLocaleDateString(
-                            "en-US",
-                            {
-                                day: 'numeric',
-                                month: 'long',
-                                year: 'numeric',
-                            }
-                        )}{" "}
-                        @ Php {item.acquisition_cost.toLocaleString()}
-                    </span>
-                    <span className="text-sm">Source: {item.source}</span>
-                    <span className="text-sm">Status: {item.status}</span>
-                    <div className="flex flex-col mt-4">
-                        <span className="text-sm font-bold">{item.quantity} in stock</span>
-                        <span className="text-sm font-bold">Php {item.value.toLocaleString()}</span>
-                    </div>
+        <div className="flex flex-col p-4 bg-gray-50 rounded-lg">
+            <span className="text-xs uppercase tracking-wider">
+                {item.itemable_type}
+            </span>
+            <span className="font-bold">{item.name}</span>
+            <span className="text-sm">
+                Acquired{" "}
+                {new Date(item.acquisition_date).toLocaleDateString("en-US", {
+                    day: "numeric",
+                    month: "long",
+                    year: "numeric",
+                })}{" "}
+                @ Php {item.acquisition_cost.toLocaleString()}
+            </span>
+            <span className="text-sm">Source: {item.source}</span>
+            <span className="text-sm">Status: {item.status}</span>
+            <div className="flex flex-col mt-4">
+                <span className="text-sm font-bold">
+                    {item.quantity} in stock
+                </span>
+                <span className="text-sm font-bold">
+                    Php {item.value.toLocaleString()}
+                </span>
+            </div>
 
-                    <div className="flex justify-end mt-4 gap-2">
-                        <Link href={route('items.edit', {id: item.id})} preserveScroll>
-                            <SecondaryButton>View</SecondaryButton>
-                        </Link>
-                    </div>
-                </div>
+            <div className="flex justify-end mt-4 gap-2">
+                <Link
+                    href={route("items.edit", { id: item.id })}
+                    preserveScroll
+                >
+                    <SecondaryButton>View</SecondaryButton>
+                </Link>
             </div>
         </div>
     );
