7e57f048d066d8daed2be6eabafe4d6bd5bcb632
Cirque0 <62349517+Cirque0@users.noreply.github.com>
2024-01-25 01:25:44 +0800

    forgot colors for responsive sidebar

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -60,10 +60,10 @@ export default function Authenticated({ user, header, children }) {
                 className={'relative sm:hidden block z-50'}
             >
                 <div className='fixed inset-0 overflow-y-auto bg-slate-800/50'>
-                    <Dialog.Panel className={'h-screen flex flex-col mr-24 bg-white p-4 gap-4'}>
+                    <Dialog.Panel className={'h-screen flex flex-col mr-24 bg-blue-500 p-4 gap-4'}>
                         <Dialog.Title className={'flex justify-between align-baseline'}>
                             <Link href="/">
-                                <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                <ApplicationLogo className="block h-9 w-auto fill-current text-white" />
                             </Link>
 
                         </Dialog.Title>
@@ -108,7 +108,7 @@ export default function Authenticated({ user, header, children }) {
                                 onClick={() => setShowingNavigationDropdown((previousState) => !previousState)}
                                 className="sm:hidden inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                             >
-                                <Bars3Icon className='h-6 w-6' />
+                                <Bars3Icon className='h-6 w-6 text-white' />
                             </button>
                             {header}
                         </div>
